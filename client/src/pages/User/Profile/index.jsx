790757bcb85c09973b142c4050d3e3f025c7b282
dev <lendahand8888@gmail.com>
2023-07-01 10:05:23 +0700

    fix KYC

--- client/src/pages/User/Profile/index.jsx
+++ client/src/pages/User/Profile/index.jsx
@@ -260,6 +260,153 @@ const Profile = () => {
                       </div>
                     </>
                   )}
+                  {status === "UNVERIFY" ? (
+                    <>
+                      <div className="flex justify-center my-4">
+                        <div className="max-w-2xl rounded-lg shadow-xl bg-gray-50">
+                          <div className="m-4">
+                            <label className="inline-block mb-2 text-gray-500">
+                              {t("idCardFront")}
+                            </label>
+                            <div className="flex flex-col items-center justify-center w-full">
+                              <label className="flex flex-col w-full h-40 border-4 border-blue-200 border-dashed hover:bg-gray-100 hover:border-gray-300">
+                                {imgFront !== "" ? (
+                                  <img
+                                    src={imgFront}
+                                    className="w-full h-full"
+                                    alt="the front of identity card"
+                                  />
+                                ) : (
+                                  <div className="flex flex-col items-center justify-center pt-7">
+                                    <svg
+                                      xmlns="http://www.w3.org/2000/svg"
+                                      className="w-8 h-8 text-gray-400 group-hover:text-gray-600"
+                                      fill="none"
+                                      viewBox="0 0 24 24"
+                                      stroke="currentColor"
+                                    >
+                                      <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth="2"
+                                        d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
+                                      />
+                                    </svg>
+                                    <p className="pt-1 text-sm tracking-wider text-gray-400 group-hover:text-gray-600">
+                                      {loadingUploadFileFront
+                                        ? "Uploading..."
+                                        : "Attach a file"}
+                                    </p>
+                                  </div>
+                                )}
+                                <input
+                                  // {...register("imgFrontData", {
+                                  //   required:
+                                  //     "The front of identity card is required",
+                                  // })}
+                                  type="file"
+                                  onChange={(e) => {
+                                    e.preventDefault();
+                                    let file = e.target.files[0];
+                                    if (file && file.type.match("image.*")) {
+                                      uploadFile(file, "front");
+                                    }
+                                  }}
+                                  accept="image/png, imgage/jpg, image/jpeg"
+                                  className="opacity-0"
+                                />
+                              </label>
+                              <p className="error-message-text">
+                                {errors.imgFrontData?.message}
+                              </p>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="flex justify-center my-4">
+                        <div className="max-w-2xl rounded-lg shadow-xl bg-gray-50">
+                          <div className="m-4">
+                            <label className="inline-block mb-2 text-gray-500">
+                              {t("idCardBack")}
+                            </label>
+                            <div className="flex flex-col items-center justify-center w-full">
+                              <label className="flex flex-col w-full h-40 border-4 border-blue-200 border-dashed hover:bg-gray-100 hover:border-gray-300">
+                                {imgBack !== "" ? (
+                                  <img
+                                    src={imgBack}
+                                    className="w-full h-full"
+                                    alt="the back of identity card"
+                                  />
+                                ) : (
+                                  <div className="flex flex-col items-center justify-center pt-7">
+                                    <svg
+                                      xmlns="http://www.w3.org/2000/svg"
+                                      className="w-8 h-8 text-gray-400 group-hover:text-gray-600"
+                                      fill="none"
+                                      viewBox="0 0 24 24"
+                                      stroke="currentColor"
+                                    >
+                                      <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth="2"
+                                        d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
+                                      />
+                                    </svg>
+                                    <p className="pt-1 text-sm tracking-wider text-gray-400 group-hover:text-gray-600">
+                                      {loadingUploadFileBack
+                                        ? "Uploading..."
+                                        : "Attach a file"}
+                                    </p>
+                                  </div>
+                                )}
+                                <input
+                                  // {...register("imgBackData", {
+                                  //   required:
+                                  //     "The back of identity card is required",
+                                  // })}
+                                  type="file"
+                                  onChange={(e) => {
+                                    e.preventDefault();
+                                    let file = e.target.files[0];
+                                    if (file && file.type.match("image.*")) {
+                                      uploadFile(file, "back");
+                                    }
+                                  }}
+                                  accept="image/png, imgage/jpg, image/jpeg"
+                                  className="opacity-0"
+                                />
+                              </label>
+                              <p className="error-message-text">
+                                {errors.imgBackData?.message}
+                              </p>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </>
+                  ) : (
+                    <>
+                      <div className="grid lg:grid-cols-2 grid-cols-1">
+                        <div className="px-4 py-2 font-semibold">
+                          {t("idCardFront")}
+                        </div>
+                        <img
+                          src={`${userInfo.imgFront}`}
+                          className="w-full px-4 py-2"
+                        />
+                      </div>
+                      <div className="grid lg:grid-cols-2 grid-cols-1">
+                        <div className="px-4 py-2 font-semibold">
+                          {t("idCardBack")}
+                        </div>
+                        <img
+                          src={`${userInfo.imgBack}`}
+                          className="w-full px-4 py-2"
+                        />
+                      </div>
+                    </>
+                  )}
                 </div>
               </div>
               <button
