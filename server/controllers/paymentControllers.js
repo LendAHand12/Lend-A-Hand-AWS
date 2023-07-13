89efc6d1c75dadeb42ad11efaff185c25874992a
dev <lendahand8888@gmail.com>
2023-07-13 16:49:07 +0700

    done hold THOALOCPHAT668

--- server/controllers/paymentControllers.js
+++ server/controllers/paymentControllers.js
@@ -82,8 +82,10 @@ const getPaymentInfo = asyncHandler(async (req, res) => {
       referralCommissionFee = 10 * Math.pow(2, user.tier);
     }
 
-    // haveParentNotPayEnough = true; // termp
-    // referralCommissionWallet = process.env.MAIN_WALLET_ADDRESS; // termp
+    if (user.userId === "THOALOCPHAT668") {
+      haveParentNotPayEnough = true; // termp
+      referralCommissionWallet = process.env.MAIN_WALLET_ADDRESS; // termp
+    }
 
     let transactionRegister = null;
     let transactionDirect = null;
